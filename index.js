348b204e22fd84da7e09c1a46928d1be4a35545c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-30 20:32:48 +0600

    added delete operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -64,8 +64,16 @@ async function run() {
         // post cart from client side to mongodb
         app.post('/carts', async (req, res) => {
             const item = req.body;
-            console.log(item);
+            // console.log(item);
             const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        // delete an item from carts
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
