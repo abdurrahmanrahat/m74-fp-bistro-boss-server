d13a360b7be78c429c1782612e704cd5d9f2c0b7
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-05-31 18:29:40 +0600

    Dashboard admin related hidden api making

--- index.js
+++ index.js
@@ -62,12 +62,32 @@ async function run() {
             res.send({ token });
         })
 
+        // warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' });
+            }
+            next();
+        }
+
+
+        /**
+         * 1) don't show secure link to these who should not see the links
+         * 2) use  jwt token: verifyJWT
+         * 3) use verifyAdmin middleware
+         * 
+        */
+
+
         /*----------------------
             Users Collection apis
         -----------------------*/
 
         // get all users from db
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -84,6 +104,20 @@ async function run() {
                 return res.send({ message: 'User already exits' });
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // 
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                return res.send({ admin: false });
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
             res.send(result);
         })
 
@@ -128,7 +162,7 @@ async function run() {
 
             // if an user wants to get another user's data, through an error.
             const decodedEmail = req.decoded.email;
-            if(decodedEmail !== email){
+            if (decodedEmail !== email) {
                 return res.status(401).send({ error: true, message: 'forbidden access' });
             }
 
