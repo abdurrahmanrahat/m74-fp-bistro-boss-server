b374ed94c03f6d6642d9c7e3a666841cd58b3d28
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-06-04 20:00:23 +0600

    Also added

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (!authorization) {
